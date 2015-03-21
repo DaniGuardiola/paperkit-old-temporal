85b8a772e5e7ad3d0b51ecb922b7efd0a847d4fe
DaniGuardiola <contacto@daniguardiola.me>
2015-03-21 19:40:31 +0100

    Upload

---
+++ source/javascript/viewport.js
@@ -0,0 +1,15 @@
+/* global Paperkit */
+Paperkit.prototype.viewport = {
+  get: function() {
+    var e = window;
+    var a = "inner";
+    if (!("innerWidth" in window)) {
+      a = "client";
+      e = document.documentElement || document.body;
+    }
+    return {
+      width: e[a + "Width"],
+      height: e[a + "Height"]
+    };
+  }
+};
